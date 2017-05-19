6ecfaa299d73138e739b714253632b078e5c3bd8
Ulyana Shoomilina <shoomilka@i.ua>
2017-05-19 17:19:16 +0300

    update delete and insert into procedures

--- 1_kp1/3_insert_update_delete.sql
+++ 1_kp1/3_insert_update_delete.sql
@@ -1,3 +1,20 @@
-INSERT INTO `users`(username,first_name,last_name,bday) VALUES ('shoomilka','Uly','Shu','1993-09-26' );;
-UPDATE `users` SET `bday`='1122-02-22' WHERE `id`=1;;
-DELETE FROM `scores` WHERE `id` = 5;;
+DROP PROCEDURE IF EXISTS `insertuser`;
+DROP PROCEDURE IF EXISTS `deleteuser`;
+DROP PROCEDURE IF EXISTS `updateuser`;
+
+delimiter //
+CREATE PROCEDURE insertuser (user varchar(10), name varchar(20), surname varchar(20), date date)
+begin
+	INSERT INTO users (username,first_name,last_name,bday) VALUES (user, name, surname, date);
+end;
+//
+CREATE PROCEDURE deleteuser (delete_id bigint(20) unsigned)
+begin
+	DELETE FROM `users` WHERE `id` = delete_id;
+end;
+//
+CREATE PROCEDURE updateuser (user varchar(10), name varchar(20), surname varchar(20), date date, user_id bigint(20) unsigned)
+begin
+	UPDATE users SET username = user, first_name = name, last_name = surname, bday = date WHERE `id` = user_id;
+end;
+//
