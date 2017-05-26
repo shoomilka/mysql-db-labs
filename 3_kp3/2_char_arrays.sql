80621b3cfc3b3028de72b6c97491cf50ecc15fcc
Ulyana Shoomilina <shoomilka@i.ua>
2017-05-26 17:08:22 +0300

    updating chararrays and numeric

--- 3_kp3/2_char_arrays.sql
+++ 3_kp3/2_char_arrays.sql
@@ -1,8 +1,14 @@
 -- Представлення з проходом рядка.
+select substr(word.word, iter.pos,1) _char
+from (select 'shoomilka' as word) word,
+(select id as pos from t10) iter
+where iter.pos <= length(word.word);
 
 -- Представлення, в якому виводяться одиночні лапки.
+select '''' as quote, 'uliana''s sweets are disappeared :-(' as sad_message;
 
 -- Представлення, в якому видаляються всі непотрібні символи.
+select username, replace(username, 's', '$') as better_view from users;
 
 -- Представлення, в якому розділяються числові та символьні дані.
 
--- 3_kp3/3_numeric.sql
+++ 3_kp3/3_numeric.sql
@@ -1,7 +1,16 @@
 -- Представлення, в якому вивести мінімальне/максимальне значення у стовпці.
+select min(id) as _min, max(id) as _max from `login_fb`;
 
 -- Представлення, яке повертає кількість записів у таблиці.
+select count(id) as _count from `login_fb`;
 
 -- Представлення, яке повертає кількість визначених (не-NULL) значень.
 
+
 -- Представлення, в якому обчислити поточну суму.
+select f2.id, f2.fb_id,
+(select sum(id) from login_fb f1
+where f1.id <= f2.id) as run_total_id,
+(select sum(fb_id) from login_fb f1
+where f1.id <= f2.id) as run_total_acc
+from login_fb f2;
