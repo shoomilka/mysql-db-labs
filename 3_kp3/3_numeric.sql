ec7e941150f7817596157456cb14b76619b43502
Ulyana Shoomilina <shoomilka@i.ua>
2017-05-26 19:12:13 +0300

    updated kp#3

--- 3_kp3/3_numeric.sql
+++ 3_kp3/3_numeric.sql
@@ -5,7 +5,22 @@ select min(id) as _min, max(id) as _max from `login_fb`;
 select count(id) as _count from `login_fb`;
 
 -- Представлення, яке повертає кількість визначених (не-NULL) значень.
-
+select count(firn) from(
+    select coalesce(ta.password, 'superstringcheckingvalue') as firn
+        from(
+            select users.first_name, login.password, login_fb.fb_id from users
+                left join login on users.id = login.user_id
+                left join login_fb on users.id = login_fb.user_id
+            union
+            select users.first_name, login.password, login_fb.fb_id from users
+                right join login on users.id = login.user_id
+                left join login_fb on users.id = login_fb.user_id
+            union
+            select users.first_name, login.password, login_fb.fb_id from users
+                right join login on users.id = login.user_id
+                right join login_fb on users.id = login_fb.user_id
+        ) ta
+    ) tab where firn <> 'superstringcheckingvalue'
 
 -- Представлення, в якому обчислити поточну суму.
 select f2.id, f2.fb_id,
